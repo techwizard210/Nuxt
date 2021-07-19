acbd5875059c116958c48bb6ddd86f595aa1d727
WB-Expert5007 <harrypotter5007hp@gmail.com>
2021-07-19 17:39:37 +0300

    fix some issue

--- components/Tab.vue
+++ components/Tab.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-        <div class="flex flex-no-wrap">
+        <div class="flex flex-no-wrap mx-5">
             <div class="flex-none w-40 h-10 rounded-l-xl bg-gray-300" />
             <div class="flex-none w-40 h-10 bg-gray-400" />
             <div class="flex-none w-40 h-10 rounded-r-xl bg-gray-500" />
